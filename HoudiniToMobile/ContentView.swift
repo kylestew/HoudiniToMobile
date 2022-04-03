9e0ee978cf5426fddb0245ab082bdf4619e0bef1
Kyle Stewart <kylestew@gmail.com>
2022-04-03 10:07:58 -0700

    Basic bun buns

--- HoudiniToMobile/ContentView.swift
+++ HoudiniToMobile/ContentView.swift
@@ -1,38 +1,88 @@
 import SwiftUI
 import SceneKit
 import SceneKit.ModelIO
+import GLTFSceneKit
+
 
 struct ContentView: View {
 
-    @State private var sunlightSwitch = true
-    @State private var cameraSwitch = true
+//    @State private var sunlightSwitch = true
+//    @State private var cameraSwitch = true
     @State private var povName = "distantCamera"
-    @State private var magnification = CGFloat(1.0)
-    @State private var isDragging = false
-    @State private var totalChangePivot = SCNMatrix4Identity
+//    @State private var magnification = CGFloat(1.0)
+//    @State private var isDragging = false
+//    @State private var totalChangePivot = SCNMatrix4Identity
 
-    private var scene = SCNScene(
-        named: "art.scnassets/bunny.scn"
-    )!
-
-//    private var scene: SCNScene!
+    private var scene: SCNScene!
 
     init() {
-//        let url = Bundle.main.url(forResource: "bunny", withExtension: "scn")!
-//        let mdlAsset = MDLAsset(url: url)
-//        let scene = SCNScene(mdlAsset: mdlAsset)
-//        self.scene = scene
+        self.scene = loadScene()!
+//        self.scene = testScene()
+        setupCamera()
+        setupLighting()
+    }
 
-//        let thing = scene.rootNode.childNode(withName: povName, recursively: true)!
+    private func loadScene() -> SCNScene? {
+        do {
+            let sceneSource = try! GLTFSceneSource(named: "scenes/bunny.glb")
+            let scene = try sceneSource.scene(options: [:])
 
-//        thing.camera?.wantsDepthOfField = true
-//        thing.camera?.focusDistance = 0.8
-//        thing.camera?.fStop = 5.6
-//        thing.camera?.wantsHDR = true
+            print("Whats in our scene?")
+            print("rootNode", scene.rootNode)
+            print("children", scene.rootNode.childNodes)
+            print("bunny", scene.rootNode.childNodes.first!)
+            print("background", scene.lightingEnvironment)
+            print("lightingEnvironment", scene.lightingEnvironment)
 
-//        thing.camera?.motionBlurIntensity = 1.0
+            return scene
+        } catch {
+            print("\(error.localizedDescription)")
+            return nil
+        }
+    }
 
-//        thing.camera?.screenSpaceAmbientOcclusionIntensity = 1.0
+    private func testScene() -> SCNScene {
+        let scene = SCNScene()
+
+        let boxGeo = SCNBox(width: 10.0, height: 10.0, length: 10.0, chamferRadius: 1.0)
+        let boxNode = SCNNode(geometry: boxGeo)
+        scene.rootNode.addChildNode(boxNode)
+
+        return scene
+    }
+
+    private func setupCamera() {
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 25)
+        cameraNode.name = _povName.wrappedValue
+        scene.rootNode.addChildNode(cameraNode)
+
+        //        let thing = scene.rootNode.childNode(withName: povName, recursively: true)!
+
+        //        thing.camera?.wantsDepthOfField = true
+        //        thing.camera?.focusDistance = 0.8
+        //        thing.camera?.fStop = 5.6
+        //        thing.camera?.wantsHDR = true
+
+        //        thing.camera?.motionBlurIntensity = 1.0
+
+        //        thing.camera?.screenSpaceAmbientOcclusionIntensity = 1.0
+    }
+
+    private func setupLighting() {
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor(white: 0.67, alpha: 1.0)
+        scene.rootNode.addChildNode(ambientLightNode)
+
+        let omniLightNode = SCNNode()
+        omniLightNode.light = SCNLight()
+        omniLightNode.light!.type = .omni
+        omniLightNode.light!.color = UIColor(white: 0.75, alpha: 1.0)
+        omniLightNode.position = SCNVector3Make(0, 50, 50)
+        scene.rootNode.addChildNode(omniLightNode)
     }
 
     var body: some View {
@@ -44,23 +94,23 @@ struct ContentView: View {
                     scene: scene,
                     pointOfView: scene.rootNode.childNode(withName: povName, recursively: true),
                     options: [.allowsCameraControl]
-//                    antialiasingMode: .none
                 )
             }
 
             VStack {
-                Text("Hello, SwiftUI!")
-                    .multilineTextAlignment(.leading)
-                    .padding()
-                    .foregroundColor(.gray)
-                    .font(.largeTitle)
+//                Text("Hello, SwiftUI!")
+//                    .multilineTextAlignment(.leading)
+//                    .padding()
+//                    .foregroundColor(.gray)
+//                    .font(.largeTitle)
+//
+//                Text("And SceneView too")
+//                    .foregroundColor(.gray)
+//                    .font(.title2)
 
-                Text("And SceneView too")
-                    .foregroundColor(.gray)
-                    .font(.title2)
+//                Spacer(minLength: 300)
 
-                Spacer(minLength: 300)
-
+                /*
                 HStack(spacing: 5) {
                     Button(action: {
                         withAnimation {
@@ -101,6 +151,7 @@ struct ContentView: View {
                             .padding()
                     }
                 }
+                 */
             }
         }
     }
